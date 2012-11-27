b89e70b7dcad58e1a873f70aaaa968f66ddb4bb6
dave <davnil74@gmail.com>
2012-11-27 14:39:10 +0100

    Fixed hiding of keyboard on leaving edit mode.

--- src/org/domain/mobile/android/mymapview/AreaDetailsEditView.java
+++ src/org/domain/mobile/android/mymapview/AreaDetailsEditView.java
@@ -2,8 +2,10 @@ package org.domain.mobile.android.mymapview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -24,6 +26,20 @@ public class AreaDetailsEditView extends LinearLayout {
 	}
 
 	
+	@Override
+	protected void onVisibilityChanged(View changedView, int visibility) {
+		if (visibility == View.GONE)
+		{
+			InputMethodManager imm = (InputMethodManager) getContext().getSystemService(
+				      Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(this.getWindowToken(), 0);
+				
+				//TODO: Move all setting of fields to here!
+				
+		}
+		super.onVisibilityChanged(changedView, visibility);
+	}
+
 	public void setFields(AreaOverlay areaOverlay) {
 		((EditText)findViewById(R.id.area_details_name_edit)).setText(areaOverlay.getName());
 		((EditText)findViewById(R.id.area_details_owner_edit)).setText(areaOverlay.getOwner());

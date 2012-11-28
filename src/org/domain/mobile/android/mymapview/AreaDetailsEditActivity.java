3f6640a7446bcc3f5b0496da2ef0fc8ce6d99f78
dave <davnil74@gmail.com>
2012-11-28 16:02:15 +0100

    Started on refactoring detail edit view to an activity.

---
+++ src/org/domain/mobile/android/mymapview/AreaDetailsEditActivity.java
@@ -0,0 +1,23 @@
+package org.domain.mobile.android.mymapview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AreaDetailsEditActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.area_details_edit);
+		getActionBar().hide();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.area_details_edit, menu);
+		return true;
+	}
+
+}
